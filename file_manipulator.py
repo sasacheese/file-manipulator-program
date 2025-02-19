5973491f38f7afa4f1a793064cbb66d1a0890ffe
sasaki <takomannto1996@gmail.com>
2025-02-19 09:01:17 +0900

    implement duplicate and replace

--- file_manipulator.py
+++ file_manipulator.py
@@ -10,9 +10,9 @@ def main():
     elif (command == 'copy'):
         copy(inputpath)
     elif (command == 'duplicate-contents'):
-        duplicate_contents()
+        duplicate_contents(inputpath)
     elif (command == 'replace-string'):
-        replace_string()
+        replace_string(inputpath)
     else:
         raise ValueError('Command is invalid')
 
@@ -21,31 +21,53 @@ def reverse(inputpath):
     outputpath = sys.argv[3]
     with open(inputpath) as f:
         contents = f.read()
-        print('####### read from ', inputpath)
+        print(f'####### read from {inputpath} #######')
         print(contents)
     with open(outputpath, 'w') as f:
         f.write(contents[::-1])
-        print('####### write into ', outputpath)
+        print(f'####### write into {outputpath} #######')
         print(contents[::-1])
-    print('\nReverse successed!! >', outputpath)
+    print(f'\nReverse successed!! > {outputpath}')
 
 def copy(inputpath):
     print('Copy', sys.argv)
     outputpath = sys.argv[3]
     with open(inputpath) as f:
         contents = f.read()
-        print('####### read from ', inputpath)
+        print(f'####### read from {inputpath} #######')
         print(contents)
     with open(outputpath, 'w') as f:
         f.write(contents)
-        print('####### write into ', outputpath)
+        print(f'####### write into {outputpath} #######')
         print(contents)
-    print('\nCopy successed!! >', outputpath)
+    print(f'\nCopy successed!! > {outputpath}')
 
-def duplicate_contents():
+def duplicate_contents(inputpath):
     print('Duplicate_contents', sys.argv)
+    n = int(sys.argv[3])
+    with open(inputpath) as f:
+        contents = f.read()
+        print(f'####### read from {inputpath} #######')
+        print(contents)
+    with open(inputpath, 'a') as f:
+        print(f'####### write into {inputpath} #######')
+        for _ in range(n):
+            f.write(contents)
+        print(f'{n} times of {contents}')
+    print(f'\nDuplicate_contents successed!! > {inputpath}')
 
-def replace_string():
+def replace_string(inputpath):
     print('Replace_string', sys.argv)
+    needle = sys.argv[3]
+    newstring = sys.argv[4]
+    with open(inputpath) as f:
+        contents = f.read()
+        print(f'####### read from {inputpath} #######')
+        print(contents)
+    with open(inputpath, 'w') as f:
+        print(f'####### write into {inputpath} #######')
+        print(contents.replace(needle, newstring))
+        f.write(contents.replace(needle, newstring))
+    print(f'\nReplace_string successed!! > {inputpath}')
 
 main()
